4932778c02fceff17c6381ba16b7b7b8b1b692d1
Henry Griffiths <42880130+henrygriffiths@users.noreply.github.com>
2023-01-25 01:17:04 -0600

    feat: many rolls (#2)

--- higher-of-many-rolls.py
+++ higher-of-many-rolls.py
@@ -1,7 +1,7 @@
 import random
 
 number_of_sides  =  int(input("How many sides on your dice? "))
-number_of_rolls  =  2
+number_of_rolls  =  int(input("How many rolls of your dice? "))
 
 sum_of_results = 0.0
 
@@ -10,7 +10,7 @@ while trials < 10**6:
     sum_of_results += max([int(random.random() * number_of_sides) + 1 for _ in range(number_of_rolls)])
     trials += 1
 
-print("Average result of rolling two and taking the highest is about {0}".format(sum_of_results/trials))
+print("Average result of rolling {1} times and taking the highest is about {0}".format(sum_of_results/trials, number_of_rolls))
 
 
 
