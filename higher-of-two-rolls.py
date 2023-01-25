5a8d846bfed2dc8990d0d4161758fb4a531da3bc
Henry Griffiths <42880130+henrygriffiths@users.noreply.github.com>
2023-01-25 01:12:48 -0600

    feat: dynamic list (#1)

--- higher-of-two-rolls.py
+++ higher-of-two-rolls.py
@@ -1,12 +1,13 @@
 import random
 
 number_of_sides  =  int(input("How many sides on your dice? "))
+number_of_rolls  =  2
 
 sum_of_results = 0.0
 
 trials = 0
 while trials < 10**6:
-    sum_of_results += max([int(random.random()*number_of_sides)+1,int(random.random()*number_of_sides)+1])
+    sum_of_results += max([int(random.random() * number_of_sides) + 1 for _ in range(number_of_rolls)])
     trials += 1
 
 print("Average result of rolling two and taking the highest is about {0}".format(sum_of_results/trials))
